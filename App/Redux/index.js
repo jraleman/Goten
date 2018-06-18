b9e1ff41d40e5b4f61bee2ae9151a332d3581c4c
jraleman <jraleman@bendiburg.com>
2018-06-17 23:38:56 -0500

    Create redux store

--- App/Redux/index.js
+++ App/Redux/index.js
@@ -1,4 +1,10 @@
-import { combineReducers } from 'redux';
+import {
+  combineReducers,
+  createStore,
+} from 'redux';
 import bookmarks from './Bookmarks/reducer';
 
 const reducers = combineReducers({ bookmarks });
+const store = createStore(reducers);
+
+export default store;
