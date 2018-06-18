2090c49dfabce9d637005d439b24fe085aef3b96
jraleman <jraleman@bendiburg.com>
2018-06-18 00:13:38 -0500

    Setup store

--- App/App.js
+++ App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 import RootContainer from './Containers/RootContainer'
+import store from './Redux';
 
 export default class App extends React.Component {
   render() {
--- App/Redux/index.js
+++ App/Redux/index.js
@@ -3,8 +3,30 @@ import {
   createStore,
 } from 'redux';
 import bookmarks from './Bookmarks/reducer';
+import {
+  loadBookmarks,
+  addBookmark,
+  removeBookmark,
+  updateBookmark
+} from './Bookmarks/actions';
 
 const reducers = combineReducers({ bookmarks });
 const store = createStore(reducers);
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(loadBookmarks());
+store.dispatch(addBookmark({
+  id: 2,
+  title: 'One more',
+  url: 'http://other.com'
+}));
+store.dispatch(updateBookmark({
+  id: 2,
+  title: 'One more edited',
+  url: 'http://other-edit.com'
+}));
+store.dispatch(removeBookmark({ id: 1 }));
+
+unsubscribe();
 
 export default store;
