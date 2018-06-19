afb22e01322c25c2bcbe12d37ed7cc5cbe9feff5
jraleman <jraleman@bendiburg.com>
2018-06-18 22:35:46 -0500

    Add AppNavigator to App container

--- App/App.js
+++ App/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './Redux';
+import AppNavigator from './Navigation/AppNavigator';
 
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <Provider store={ store }>
-        <RootContainer />
+        <AppNavigator />
       </Provider>
     );
   }
 }
+
+export default App;
