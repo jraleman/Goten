100a6b4c1054377ab5d54c102b7ab6001129d191
jraleman <jraleman@bendiburg.com>
2018-06-18 20:33:11 -0500

    Import Redux Provider into App

--- App/App.js
+++ App/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
-import RootContainer from './Containers/RootContainer'
+import { Provider } from 'react-redux';
 import store from './Redux';
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1 }}>
+      <Provider store={ store }>
         <RootContainer />
-      </View>
+      </Provider>
     );
   }
 }
