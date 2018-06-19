e3dcf4ba38b2b054fef1aabb48fe2f8c55d85c5b
jraleman <jraleman@bendiburg.com>
2018-06-19 17:03:31 -0500

    Add network-connectivity actions

---
+++ App/Redux/Modules/Network/actions.js
@@ -0,0 +1,12 @@
+export const NETWORK_CHANGE = 'network/change';
+
+export function setConnectivity(network) {
+  const status = network.toUpperCase();
+  return ({
+    type: NETWORK_CHANGE,
+    payload: {
+      isOnline: status !== 'NONE',
+      isOffline: status === 'NONE'
+    }
+  });
+};
