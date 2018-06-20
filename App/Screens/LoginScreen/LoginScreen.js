b18943ee6ccad1068e67adbd4b9fc7575601f3ab
jraleman <jraleman@bendiburg.com>
2018-06-20 03:50:43 -0500

    Add dummy LoginScreen

---
+++ App/Screens/LoginScreen/LoginScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  Container,
+  Content,
+  Text
+} from 'native-base';
+import styles from './styles';
+
+class LoginScreen extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+    return;
+  }
+  render () {
+    return (
+      <Container style={ styles.container }>
+        <Content>
+          <Text>{ "LoginScreen" }</Text>
+        </Content>
+      </Container>
+    );
+  }
+}
