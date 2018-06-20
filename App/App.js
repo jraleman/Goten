005bbd17c4fba243495bbb56e0623e4041d2e814
jraleman <jraleman@bendiburg.com>
2018-06-19 19:27:16 -0500

    Remove commented stuff

--- App/App.js
+++ App/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
 import store from './Redux';
-// import AppNavigator from './Navigation/AppNavigator';
-// import NetworkIndicator from './Components/NetworkIndicator';
 import RootContainer from './Containers/RootContainer';
 
 class App extends React.Component {
--- App/Containers/RootContainer/RootContainer.js
+++ App/Containers/RootContainer/RootContainer.js
@@ -1,15 +1,8 @@
 import React from 'react';
-// import {
-  // Platform,
-  // Text,
-  // View
-// } from 'react-native';
 import { Container } from 'native-base';
 import styles from './styles';
-// import TestScreen from '../../Screens/TestScreen';
 import AppNavigator from '../../Navigation/AppNavigator';
 import NetworkIndicator from '../../Components/NetworkIndicator';
-
 
 class RootContainer extends React.Component {
   render() {
