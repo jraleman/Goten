9904d1869aadb43c5568571222d9aa10c0f2c211
jraleman <jraleman@bendiburg.com>
2018-06-26 04:56:28 -0500

    Import native-base components, add header

--- App/Screens/NativeBaseScreen/NativeBaseScreen.js
+++ App/Screens/NativeBaseScreen/NativeBaseScreen.js
@@ -1,7 +1,48 @@
 import React from 'react';
 import {
+  Badge,
+  Body,
+  Button,
+  Card,
+  CardItem,
+  CheckBox,
   Container,
-  Content
+  Content,
+  Fab,
+  Footer,
+  FooterTab,
+  Form,
+  H1,
+  H2,
+  H3,
+  Header,
+  Icon,
+  IconNB,
+  Input,
+  InputGroup,
+  Item,
+  Label,
+  Left,
+  ListItem,
+  Picker,
+  Radio,
+  Right,
+  Segment,
+  Separator,
+  Spinner,
+  Subtitle,
+  SwipeRow,
+  Switch,
+  Tab,
+  TabBar,
+  TabContainer,
+  TabHeading,
+  Text,
+  Textarea,
+  Thumbnail,
+  Title,
+  Toast,
+  View
 } from 'native-base';
 import styles from './styles';
 
@@ -14,8 +55,19 @@ class NativeBaseScreen extends React.Component {
   render () {
     return (
       <Container style={ styles.container }>
-        <Content>
-        </Content>
+
+        <Header>
+          <Left>
+            <Button transparent onPress={() => this.props.navigation.goBack()}>
+              {/*<Icon name="arrow-back" />*/}
+            </Button>
+          </Left>
+          <Body>
+            <Title>{ "NativeBaseScreen" }</Title>
+          </Body>
+          <Right />
+        </Header>
+
       </Container>
     );
   }
