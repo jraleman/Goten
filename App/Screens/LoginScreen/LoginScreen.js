d72948ec33fc8ae8f4fdd02de944157009773050
jraleman <jraleman@bendiburg.com>
2018-06-25 22:26:47 -0500

    Add Email and Password Input to LoginScreen

--- App/Screens/LoginScreen/LoginScreen.js
+++ App/Screens/LoginScreen/LoginScreen.js
@@ -16,12 +16,49 @@ class LoginScreen extends React.Component {
     };
     return;
   }
+  hideForm = async () => {
+    if (this.buttonRef && this.formRef && this.linkRef) {
+      await Promise.all([
+        this.buttonRef.zoomOut(200),
+        this.formRef.fadeOut(300),
+        this.linkRef.fadeOut(300)
+      ])
+    }
+  }
   render () {
+    //const { email, password } = this.state
+    //const { isLoading, onSignupLinkPress, onLoginPress } = this.props
+    //const isValid = email !== '' && password !== ''
     return (
       <View style={ styles.container }>
-        <View style={ styles.form }>
-          <Text>{ "LoginScreen" }</Text>
-          <AnimatedTextInput />
+        <View
+          style={ styles.form }
+          ref={(ref) => { this.formRef = ref }}
+        >
+          {/* EmailInput */}
+          <AnimatedTextInput
+            blurOnSubmit={ false }
+            editable={ true }
+            isEmail={ true }
+            name={ 'email' }
+            onChangeText={ (value) => this.setState({ email: value })}
+            onSubmitEditing={ () => this.passwordInputRef.focus() }
+            placeholder={ 'Email' }
+            ref={ (ref) => this.emailInputRef = ref }
+            withRef={ true }
+          />
+          {/* PasswordInput */}
+          <AnimatedTextInput
+            editable={ true }
+            isPassword={ true }
+            name={ 'password' }
+            onChangeText={ (value) => this.setState({ password: value })}
+            onSubmitEditing={ () => this.passwordInputRef.focus() }
+            placeholder={ 'Password' }
+            ref={ (ref) => this.passwordInputRef = ref }
+            returnKeyType={'done'}
+            withRef={ true }
+          />
         </View>
       </View>
     );
