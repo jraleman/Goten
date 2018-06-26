5e1ffe822f719ec6f175b96d55244db2292577e2
jraleman <jraleman@bendiburg.com>
2018-06-26 03:36:44 -0500

    Add NativeBaseScreen to initialRouteName

--- App/Navigation/AppNavigator/AppNavigator.js
+++ App/Navigation/AppNavigator/AppNavigator.js
@@ -5,20 +5,23 @@ import { StackNavigator } from 'react-navigation';
 import TestScreen from '../../Screens/TestScreen';
 import LoginScreen from '../../Screens/LoginScreen';
 import CategoriesScreen from '../../Screens/CategoriesScreen';
+import NativeBaseScreen from '../../Screens/NativeBaseScreen';
 
 // 'float'  -> Stays at the top and animates as screens are changed.
 // 'screen' -> Each screen has a header attached to it,
 //             and the header fades in and out together with the screen.
-const mode = (Platform.OS === 'android' ? 'screen' : 'float');
+// const mode = (Platform.OS === 'android' ? 'screen' : 'float');
+const mode = 'none';
 
 export default StackNavigator(
   {
     'Login': { screen: LoginScreen },
     'Test': { screen: TestScreen },
-    'Categories': { screen: CategoriesScreen }
+    'Categories': { screen: CategoriesScreen },
+    'NativeBase': { screen: NativeBaseScreen }
   },
   {
-    initialRoute: 'Login',
+    initialRouteName: 'NativeBase',
     headerMode: mode
   }
 );
