b9bae58e84bbd26d91e9b84fb845747082545254
jraleman <jraleman@bendiburg.com>
2018-06-25 20:05:59 -0500

    Add AnimatedTextInput prop-types

--- App/Components/AnimatedTextInput/AnimatedTextInput.js
+++ App/Components/AnimatedTextInput/AnimatedTextInput.js
@@ -1,4 +1,5 @@
 import React from  'react';
+import PropTypes from 'prop-types';
 import { TextInput } from 'react-native';
 import { View } from 'react-native-animatable';
 import styles from './styles';
@@ -16,6 +17,11 @@ class AnimatedTextInput extends React.Component {
       </View>
     );
   }
+}
+
+AnimatedTextInput.propTypes = {
+  isEnabled: PropTypes.bool,
+  isPassword: PropTypes.bool
 }
 
 export default AnimatedTextInput;
