bafcbbb21d867eb87751900926b0161c6a407924
jraleman <jraleman@bendiburg.com>
2018-06-26 04:52:42 -0500

    Add native-base StyleProvider in RootContainer

--- App/Containers/RootContainer/RootContainer.js
+++ App/Containers/RootContainer/RootContainer.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Container } from 'native-base';
-import styles from './styles';
+import { StyleProvider, Container } from 'native-base';
 import AppNavigator from '../../Navigation/AppNavigator';
 import NetworkIndicator from '../../Components/NetworkIndicator';
+import styles from './styles';
+import variable from '../../Themes/variable';
+import getTheme from '../../Themes/NativeBaseComponents';
 
 class RootContainer extends React.Component {
   render() {
     return (
-      <Container style={ styles.container }>
-        <NetworkIndicator />
-        <AppNavigator />
-      </Container>
+      <StyleProvider style={ getTheme(variable) }>
+        <Container style={ styles.container }>
+          <NetworkIndicator />
+          <AppNavigator />
+        </Container>
+      </StyleProvider>
     );
   }
 }
