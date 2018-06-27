626392139986bd6ad21760a8018a5094569b22f3
jraleman <jraleman@bendiburg.com>
2018-06-26 19:17:54 -0500

    Style BadgesContainer code :)

--- App/Containers/NativeBaseContainers/styles.js
+++ App/Containers/NativeBaseContainers/styles.js
@@ -3,5 +3,17 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   mb: {
     marginBottom: 10
+  },
+  mb15: {
+    marginBottom: 15
+  },
+  badgeCustom: {
+    backgroundColor: 'black',
+    color: 'white'
+  }
+  badgeIcon: {
+    fontSize: 15,
+    color: "#fff",
+    lineHeight: 20
   }
 });
