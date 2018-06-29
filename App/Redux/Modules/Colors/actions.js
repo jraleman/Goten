9841613615ecac14eb3f804be6106097deac4188
jraleman <jraleman@bendiburg.com>
2018-06-28 20:13:22 -0500

    Add redux module loadColor action

---
+++ App/Redux/Modules/Colors/actions.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+// Dispatch the color in a string, and add a '#' before it.
+// This is to manage the color easier, so they are in the correct format.
+// Example: '29a9d2' - > '#29a9d2'
+export function loadColor () {
+  return (dispatch) => {
+    return (
+      axios.get("http://colr.org/json/color/random")
+      .then((response) => {
+        dispatch(changeColor('#' + response.data.new_color));
+      });
+    );
+  }
+};
