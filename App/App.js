09ea4378525738f52cc71a64ae265f9ed186d87b
jraleman <jraleman@bendiburg.com>
2018-06-29 15:38:53 -0500

    Add firstLaunch state and AsyncStorage via componentDidMount lifecycle :D

--- App/App.js
+++ App/App.js
@@ -3,7 +3,28 @@ import { Provider } from 'react-redux';
 import store from './Redux';
 import RootContainer from './Containers/RootContainer';
 
+// Thanks for the help, martinarroyo.
+// Source: https://stackoverflow.com/a/40729761
 class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      firstLaunch: null
+    };
+  }
+  componentDidMount () {
+    AyncStorage.getItem('alreadyLaunched')
+    .then(value => {
+      if (value == null) {
+        AsyncStorage.setItem('alreadyLaunched', true);
+        this.setState({ firstLaunch: true });
+      }
+      else {
+        this.setState({ firstLaunch: false });
+        // ... you can also add your error handling code here :)
+      }
+    })
+  }
   render() {
     return (
       <Provider store={ store }>
