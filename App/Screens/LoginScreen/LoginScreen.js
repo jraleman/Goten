c9b779e471a332aa39592ae19bde4c454e409604
jraleman <jraleman@bendiburg.com>
2018-06-30 20:12:59 -0500

    Strip down LoginScreen

--- App/Screens/LoginScreen/LoginScreen.js
+++ App/Screens/LoginScreen/LoginScreen.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {
-  Text,
-  View
-} from 'react-native-animatable';
+  Container,
+  Content,
+  Text
+} from 'native-base'
 import styles from './styles';
-import AnimatedTextInput from '../../Components/AnimatedTextInput';
-import AnimatedButton from '../../Components/AnimatedButton';
 
 class LoginScreen extends React.Component {
   constructor (props) {
@@ -16,52 +15,12 @@ class LoginScreen extends React.Component {
     };
     return;
   }
-  // hideForm = async () => {
-  //   if (this.buttonRef && this.formRef && this.linkRef) {
-  //     await Promise.all([
-  //       this.buttonRef.zoomOut(200),
-  //       this.formRef.fadeOut(300),
-  //       this.linkRef.fadeOut(300)
-  //     ])
-  //   }
-  // }
   render () {
-    //const { email, password } = this.state
-    //const { isLoading, onSignupLinkPress, onLoginPress } = this.props
-    //const isValid = email !== '' && password !== ''
     return (
-      <View style={ styles.container }>
-        <View
-          style={ styles.form }
-          ref={(ref) => { this.formRef = ref }}
-        >
-          {/* EmailInput */}
-          <AnimatedTextInput
-            blurOnSubmit={ false }
-            editable={ true }
-            isEmail={ true }
-            name={ 'email' }
-            onChangeText={ (value) => this.setState({ email: value })}
-            onSubmitEditing={ () => this.passwordInputRef.focus() }
-            placeholder={ 'Email' }
-            ref={ (ref) => this.emailInputRef = ref }
-            withRef={ true }
-          />
-          {/* PasswordInput */}
-          <AnimatedTextInput
-            editable={ true }
-            isPassword={ true }
-            name={ 'password' }
-            onChangeText={ (value) => this.setState({ password: value })}
-            onSubmitEditing={ () => this.passwordInputRef.focus() }
-            placeholder={ 'Password' }
-            ref={ (ref) => this.passwordInputRef = ref }
-            returnKeyType={'done'}
-            withRef={ true }
-          />
-          <AnimatedButton />
-        </View>
-      </View>
+      <Container style={ styles.container }>
+        <Content>
+        </Content>
+      </Container>
     );
   }
 }
--- App/Screens/LoginScreen/styles.js
+++ App/Screens/LoginScreen/styles.js
@@ -1,21 +1,7 @@
 import { StyleSheet } from 'react-native';
 
-// Move this to metrics.js
-import { Dimensions, Platform } from 'react-native'
-const IS_ANDROID = Platform.OS === 'android'
-const { height, width } = Dimensions.get('window')
-export const metrics = {
-  ANDROID_STATUSBAR: 24,
-  DEVICE_HEIGHT: IS_ANDROID ? height - 24 : height,
-  DEVICE_WIDTH: width
-}
-
 export default StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: metrics.DEVICE_WIDTH * 0.1
-  },
-  form: {
-    marginTop: 20
   }
 });
