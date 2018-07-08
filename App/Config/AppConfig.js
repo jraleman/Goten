41231e786b56798824b6712412e6bff3bb3f6fb4
jraleman <jraleman@bendiburg.com>
2018-07-08 11:22:22 -0700

    Add font-scaling and i18n app config file

---
+++ App/Config/AppConfig.js
@@ -0,0 +1,6 @@
+import '../I18n/I18n';
+
+export default {
+  // React native font scaling override default is on (true)
+  allowTextFontScaling: true
+};
---
+++ App/Config/index.js
@@ -0,0 +1,4 @@
+import { Text } from 'react-native';
+
+// Allow font-scaling in app
+Text.defaultProps.allowFontScaling = AppConfig.allowTextFontScaling;
