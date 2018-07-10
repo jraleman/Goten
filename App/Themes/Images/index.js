53f1a8bb8f78b513bf01f9f6e89f99798569a2e9
jraleman <jraleman@bendiburg.com>
2018-07-10 00:46:24 -0700

    Add onboarding screen images

--- App/Themes/Images/index.js
+++ App/Themes/Images/index.js
@@ -1,4 +1,8 @@
 const Images = {
+  onboardingSlideOne: require("./Source/slide-one.png"),
+  onboardingSlideTwo: require("./Source/slide-two.png"),
+  onboardingSlideThree: require("./Source/slide-three.png"),
+  onboardingSlideFour: require("./Source/slide-four.png"),
   loginBackground: require("./Source/login-background.jpg"),
   logoLandscape: require("./Source/logo-landscape.png")
 }
