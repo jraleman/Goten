5c75395c224b0165e2cc5d7c4ebf42295a767f55
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-07-17 04:11:41 -0700

    Add DrawerNavigation base configuration

--- App/Navigation/DrawerNavigator/DrawerNavigator.js
+++ App/Navigation/DrawerNavigator/DrawerNavigator.js
@@ -1,5 +1,18 @@
 import { DrawerNavigator } from 'react-navigation';
+// import DrawerNavigatorContainer from '../../Containers/DrawerNavigatorContainer';
+import CategoriesScreen from '../../Screens/CategoriesScreen';
+import ColorsScreen from '../../Screens/ColorsScreen';
+import NativeBaseScreen from '../../Screens/NativeBaseScreen';
+import TestScreen from '../../Screens/TestScreen';
 
-const DrawerNavigator = DrawerNavigator(({}), ({}))
-
-export default DrawerNavigator;
+const DrawerNavigation = DrawerNavigator({
+    'Categories': { screen: CategoriesScreen },
+    'Colors': { screen: ColorsScreen },
+    'Native Base': { screen: NativeBaseScreen },
+    'Test': { screen: TestScreen },
+  },
+  {
+    initialRouteName: 'Native Base',
+    // contentComponent: props => (<DrawerNavigatorContainer { ...props } />)
+  }
+);
