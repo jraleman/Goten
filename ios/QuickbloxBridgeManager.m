7cddb677154ad0a80bfeb7f33460cf67c811d821
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-07-23 15:38:39 -0700

    Add dummy QuickbloxBridge

---
+++ ios/QuickbloxBridgeManager.m
@@ -0,0 +1,41 @@
+//  Created by react-native-create-bridge
+
+#import <Foundation/Foundation.h>
+#import "QuickbloxBridge.h"
+#import "QuickbloxBridgeManager.h"
+
+// import RCTBridge
+#if __has_include(<React/RCTBridge.h>)
+#import <React/RCTBridge.h>
+#elif __has_include(“RCTBridge.h”)
+#import “RCTBridge.h”
+#else
+#import “React/RCTBridge.h” // Required when used as a Pod in a Swift project
+#endif
+
+@implementation QuickbloxBridgeManager
+
+@synthesize bridge = _bridge;
+
+// Export a native module
+// https://facebook.github.io/react-native/docs/native-modules-ios.html
+RCT_EXPORT_MODULE();
+
+// Return the native view that represents your React component
+- (UIView *)view
+{
+  return [[QuickbloxBridge alloc] initWithEventDispatcher:self.bridge.eventDispatcher];
+}
+
+RCT_EXPORT_VIEW_PROPERTY(exampleProp, NSString)
+
+// Export constants
+// https://facebook.github.io/react-native/releases/next/docs/native-modules-ios.html#exporting-constants
+- (NSDictionary *)constantsToExport
+{
+  return @{
+           @"EXAMPLE": @"example"
+         };
+}
+
+@end
