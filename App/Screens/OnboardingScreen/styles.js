395354a58b4b7c5d36b444f9875ce2ece8cd71a7
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-09-01 00:49:59 -0700

    Fix imageStyle missing image issue

--- App/Screens/OnboardingScreen/styles.js
+++ App/Screens/OnboardingScreen/styles.js
@@ -2,14 +2,16 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: "space-between"
   },
   imageStyle: {
-    flex: 1,
-    height: undefined,
-    width: undefined
+    width: 220,
+    height: 220,
   },
   textStyle: {
+    marginBottom: 20,
     color: '#f9f9f9'
   },
   titleStyle: {
