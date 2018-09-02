7bdeb6e507cc93fe8689279807f9c4d346f190e2
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-09-02 15:58:45 -0700

    Add, link, and attribute drawer-navigation-avatar image file

--- App/Themes/Images/index.js
+++ App/Themes/Images/index.js
@@ -6,7 +6,8 @@ const Images = {
   loginBackground: require("./Source/login-background.jpg"),
   logoWhite: require("./Source/logo-white.png"),
   logoBlack: require("./Source/logo-black.png"),
-  drawerNavigationCover: require("./Source/drawer-navigation-cover.jpg")
+  drawerNavigationCover: require("./Source/drawer-navigation-cover.jpg"),
+  drawerNavigationAvatar: require("./Source/drawer-navigation-avatar.png")
 }
 
 export default Images;
