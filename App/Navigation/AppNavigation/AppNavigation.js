fd6415e733219d411b7352f6ff43e2b1095bf7b7
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-09-09 22:15:18 -0700

    Link new screens to AppNavigation

--- App/Navigation/AppNavigation/AppNavigation.js
+++ App/Navigation/AppNavigation/AppNavigation.js
@@ -1,13 +1,30 @@
+#!/usr/bin/env node
+
+/**
+ *      |    o             |
+ * ,---.|---..,---.,---. --|--- ,---.
+ * ,---||   |||---'|       |    |   |
+ * `---^`---'``---'`    o  `---'`---'
+ *
+ * @author [Jose Ramon Aleman](https://github.com/jraleman)
+ */
+
+// ~~ Dependencies ~~
+import React from 'react';
 import { StackNavigator } from 'react-navigation';
-
-import TestScreen from '../../Screens/TestScreen';
-import LoginScreen from '../../Screens/LoginScreen';
-import SignupScreen from '../../Screens/SignupScreen';
-import ForgotPasswordScreen from '../../Screens/ForgotPasswordScreen';
-import ThemeScreen from '../../Screens/ThemeScreen';
-import OnboardingScreen from '../../Screens/OnboardingScreen';
-
+// ~~ Local Dependencies ~~
+import CalendarScreen from '../../Screens/CalendarScreen';
 import DrawerNavigation from '../DrawerNavigation';
+import ForgotPasswordScreen from '../../Screens/ForgotPasswordScreen';
+import LoginScreen from '../../Screens/LoginScreen';
+import OnboardingScreen from '../../Screens/OnboardingScreen';
+import ProfileScreen from '../../Screens/ProfileScreen';
+import SearchScreen from '../../Screens/SearchScreen';
+import SettingsScreen from '../../Screens/SettingsScreen';
+import SignupScreen from '../../Screens/SignupScreen';
+import TestScreen from '../../Screens/TestScreen';
+import ThemeScreen from '../../Screens/ThemeScreen';
+import VideoCallScreen from '../../Screens/VideoCallScreen';
 
 // 'float'  -> Stays at the top and animates as screens are changed.
 // 'screen' -> Each screen has a header attached to it,
@@ -17,14 +34,19 @@ const mode = 'none';
 
 const AppNavigation = StackNavigator(
   {
-    'Login': { screen: LoginScreen },
-    'Signup': { screen: SignupScreen },
-    'ForgotPassword': { screen: ForgotPasswordScreen },
-    'Test': { screen: TestScreen },
-    'Theme': { screen: ThemeScreen },
-    'Onboarding': { screen: OnboardingScreen },
-    'MainDrawer': { screen: DrawerNavigation.Main },
-    'AltDrawer': { screen: DrawerNavigation.Alt }
+    AltDrawer: { screen: DrawerNavigation.Alt },
+    Calendar: { screen: CalendarScreen },
+    ForgotPassword: { screen: ForgotPasswordScreen },
+    Login: { screen: LoginScreen },
+    MainDrawer: { screen: DrawerNavigation.Main },
+    Onboarding: { screen: OnboardingScreen },
+    Profile: { screen: ProfileScreen },
+    Search: { screen: SearchScreen },
+    Settings: { screen: SettingsScreen },
+    Signup: { screen: SignupScreen },
+    Test: { screen: TestScreen },
+    Theme: { screen: ThemeScreen },
+    VideoCall: { screen: VideoCallScreen }
   },
   {
     initialRouteName: 'Onboarding',
