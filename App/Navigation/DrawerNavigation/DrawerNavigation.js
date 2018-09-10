f6a6856bd5dab0813c4e01ff5cc9cc098c62c4df
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-09-09 22:21:54 -0700

    Link new screens to DrawerNavigation

--- App/Navigation/DrawerNavigation/DrawerNavigation.js
+++ App/Navigation/DrawerNavigation/DrawerNavigation.js
@@ -13,24 +13,47 @@
 import React from 'react';
 import { DrawerNavigator } from 'react-navigation';
 // ~~ Local Dependencies ~~
+import CalendarScreen from '../../Screens/CalendarScreen';
+import DrawerNavigation from '../DrawerNavigation';
 import DrawerNavigationContainer from '../../Containers/DrawerNavigationContainer';
+import ForgotPasswordScreen from '../../Screens/ForgotPasswordScreen';
+import LoginScreen from '../../Screens/LoginScreen';
+import OnboardingScreen from '../../Screens/OnboardingScreen';
+import ProfileScreen from '../../Screens/ProfileScreen';
+import SearchScreen from '../../Screens/SearchScreen';
+import SettingsScreen from '../../Screens/SettingsScreen';
+import SignupScreen from '../../Screens/SignupScreen';
 import TestScreen from '../../Screens/TestScreen';
 import ThemeScreen from '../../Screens/ThemeScreen';
+import VideoCallScreen from '../../Screens/VideoCallScreen';
 
-const MainDrawerNav = DrawerNavigator({
-  'Theme': { screen: ThemeScreen },
-  'Test': { screen: TestScreen }
-}, {
-  initialRouteName: 'Theme',
-  contentComponent: props => <DrawerNavigationContainer { ...props } />
-});
+const MainDrawerNav = DrawerNavigator(
+  {
+    'Calendar': { screen: CalendarScreen },
+    'Profile': { screen: ProfileScreen },
+    'Search': { screen: SearchScreen },
+    'Test': { screen: TestScreen },
+    'Theme': { screen: ThemeScreen },
+    'Video Call': { screen: VideoCallScreen }
+  },
+  {
+    initialRouteName: 'Search',
+    contentComponent: props => <DrawerNavigationContainer { ...props } />
+  }
+);
 
-const AltDrawerNav = DrawerNavigator({
-  'Test': { screen: TestScreen }
-}, {
-  initialRouteName: 'Test',
-  contentComponent: props => <DrawerNavigationContainer { ...props } />
-});
+const AltDrawerNav = DrawerNavigator(
+  {
+    'Profile': { screen: ProfileScreen },
+    'Search': { screen: SearchScreen },
+    'Test': { screen: TestScreen },
+    'Theme': { screen: ThemeScreen },
+  },
+  {
+    initialRouteName: 'Calendar',
+    contentComponent: props => <DrawerNavigationContainer { ...props } />
+  }
+);
 
 const DrawerNavigation = {
   Main: MainDrawerNav,
