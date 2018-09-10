fbfd492993f63988eef273a6b35ef4334dbcc350
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-09-09 22:01:26 -0700

    Add dummy CalendarScreen

---
+++ App/Screens/CalendarScreen/index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+/**
+ * CalendarScreen/index.js
+ * @author [Jose Ramon Aleman](https://github.com/jraleman)
+ */
+
+import CalendarScreen from './CalendarScreen';
+export default CalendarScreen;
