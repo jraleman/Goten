84a619d394ba3082cbab59b0f5978aab8ffc1d67
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-09-09 22:05:36 -0700

    Add dummy VideoCallScreen

---
+++ App/Screens/VideoCallScreen/index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+/**
+ * VideoCallScreen/index.js
+ * @author [Jose Ramon Aleman](https://github.com/jraleman)
+ */
+
+import VideoCallScreen from './VideoCallScreen';
+export default VideoCallScreen;
