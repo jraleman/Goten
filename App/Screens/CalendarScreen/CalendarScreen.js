267be8c6393e15339395fecb0ad1c50f68a1498f
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-09-11 17:50:35 -0700

    Add Agenda example to CalendarScreen

--- App/Screens/CalendarScreen/CalendarScreen.js
+++ App/Screens/CalendarScreen/CalendarScreen.js
@@ -29,33 +29,80 @@ import AppHeader from '../../Components/AppHeader';
  * @extends { React.Component }
  */
 class CalendarScreen extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       items: {}
     };
-    return;
   }
-  /**
-   * Renders the component.
-   * @return { string } - RN markup for the component
-   */
-  render () {
+
+  loadItems(day) {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 5);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: 'Item for ' + strTime,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      //console.log(this.state.items);
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+    // console.log(`Load Items for ${day.year}-${day.month}`);
+  }
+
+  renderItem(item) {
+    return (
+      <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
+    );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  }
+  render() {
     return (
       <React.Fragment>
-        <Container style={ styles.container }>
+        <Container>
           <AppHeader
             title={ 'Calendar' }
-            onPress={ () => this.props.navigation.openDrawer() }
             icon={ 'menu' }
+            onPress={ () => this.props.navigation.openDrawer() }
           />
-          <Content style={ styles.content }>
-            <Agenda />
-          </Content>
+          <Agenda
+            items={this.state.items}
+            loadItemsForMonth={this.loadItems.bind(this)}
+            selected={'2017-05-16'}
+            renderItem={this.renderItem.bind(this)}
+            renderEmptyDate={this.renderEmptyDate.bind(this)}
+            rowHasChanged={this.rowHasChanged.bind(this)}
+          />
         </Container>
       </React.Fragment>
     );
   }
-};
+}
 
 export default CalendarScreen;
--- App/Screens/CalendarScreen/styles.js
+++ App/Screens/CalendarScreen/styles.js
@@ -16,11 +16,18 @@ import { Colors } from '../../Themes';
  */
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
   },
-  content: {
-    flex: 1
+  emptyDate: {
+    height: 15,
+    flex:1,
+    paddingTop: 30
   }
 });
 
