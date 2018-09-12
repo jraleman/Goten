fbd32f681f6b4f6b323f27e8844ee2cfb5424d2c
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-09-11 21:22:31 -0700

    Add react-native-search-filter example to SearchScreen

--- App/Screens/SearchScreen/SearchScreen.js
+++ App/Screens/SearchScreen/SearchScreen.js
@@ -12,13 +12,65 @@
 
 // ~~ Dependencies ~~
 import React from 'react';
-import {
-  Container,
-  Content,
-  Text,
-} from 'native-base';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import SearchInput, { createFilter } from 'react-native-search-filter';
+
 // ~~ Local Dependencies ~~
 import styles from './styles';
+
+const KEYS_TO_FILTERS = ['user.name', 'subject'];
+const emails = [{
+  id: 1,
+  user: {
+    name: 'Juniper',
+  },
+  subject: 'Hello World!',
+}, {
+  id: 2,
+  user: {
+    name: 'Robert'
+  },
+  subject: 'React is <3',
+}, {
+  id: 3,
+  user: {
+    name: 'you can search for me using a regex : `java$`'
+  },
+  subject: "What's Up?",
+}
+  , {
+  id: 4,
+  user: {
+    name: 'Georgia'
+  },
+  subject: 'How are you today?',
+}, {
+  id: 5,
+  user: {
+    name: 'Albert'
+  },
+  subject: 'Hey!',
+}, {
+  id: 6,
+  user: {
+    name: 'Zoey'
+  },
+  subject: 'React Native!',
+}, {
+  id: 7,
+  user: {
+    name: 'Cody'
+  },
+  subject: 'is super!',
+}, {
+  id: 8,
+  user: {
+    name: 'Chili'
+  },
+  subject: 'Awesome!',
+}]
+
+
 
 /**
  * Container to render when DrawerNavigation is called.
@@ -26,26 +78,39 @@ import styles from './styles';
  * @extends { React.Component }
  */
 class SearchScreen extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {};
-    return;
+  constructor(props) {
+      super(props);
+      this.state = {
+        searchTerm: ''
+      }
+    }
+    searchUpdated(term) {
+      this.setState({ searchTerm: term })
+    }
+    render() {
+      const filteredEmails = emails.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
+      return (
+        <View style={styles.container}>
+          <SearchInput
+            onChangeText={(term) => { this.searchUpdated(term) }}
+            style={styles.searchInput}
+            placeholder="Type a message to search"
+            />
+          <ScrollView>
+            {filteredEmails.map(email => {
+              return (
+                <TouchableOpacity onPress={()=>alert(email.user.name)} key={email.id} style={styles.emailItem}>
+                  <View>
+                    <Text>{email.user.name}</Text>
+                    <Text style={styles.emailSubject}>{email.subject}</Text>
+                  </View>
+                </TouchableOpacity>
+              )
+            })}
+          </ScrollView>
+        </View>
+      );
   }
-  /**
-   * Renders the component.
-   * @return { string } - RN markup for the component
-   */
-  render () {
-    return (
-      <React.Fragment>
-        <Container style={ styles.container }>
-          <Content style={ styles.content }>
-            <Text>{ "SearchScreen" }</Text>
-          </Content>
-        </Container>
-      </React.Fragment>
-    );
-  }
-};
+}
 
 export default SearchScreen;
