0db0f954e6e8670e8ebb6d5ec4f7aad3df5f0cc9
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-09-12 20:17:11 -0700

    Copypasta nativebase picker sample to SearchScreen

--- App/Screens/SearchScreen/SearchScreen.js
+++ App/Screens/SearchScreen/SearchScreen.js
@@ -16,6 +16,10 @@ import {
   Container,
   Content,
   Text,
+  Form,
+  Item,
+  Icon,
+  Picker,
 } from 'native-base';
 // ~~ Local Dependencies ~~
 import AppHeader from '../../Components/AppHeader';
@@ -29,8 +33,14 @@ import styles from './styles';
 class SearchScreen extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      selected: undefined
+    };
     return;
+  }
+  onValueChange(value: string) {
+    this.setState({ selected: value });
+    return ;
   }
   /**
    * Renders the component.
@@ -46,7 +56,30 @@ class SearchScreen extends React.Component {
             onPress={ () => this.props.navigation.openDrawer() }
           />
           <Content style={ styles.content }>
-            <Text>{ "SearchScreen" }</Text>
+            <Form>
+              <Picker
+                mode="dropdown"
+                placeholder="Select your SIM"
+                iosIcon={<Icon name="ios-arrow-down-outline" />}
+                placeholder="Select your SIM"
+                textStyle={{ color: "#5cb85c" }}
+                itemStyle={{
+                  backgroundColor: "#d3d3d3",
+                  marginLeft: 0,
+                  paddingLeft: 10
+                }}
+                itemTextStyle={{ color: '#788ad2' }}
+                style={{ width: undefined }}
+                selectedValue={this.state.selected}
+                onValueChange={this.onValueChange.bind(this)}
+              >
+                <Picker.Item label="Wallet" value="key0" />
+                <Picker.Item label="ATM Card" value="key1" />
+                <Picker.Item label="Debit Card" value="key2" />
+                <Picker.Item label="Credit Card" value="key3" />
+                <Picker.Item label="Net Banking" value="key4" />
+              </Picker>
+            </Form>
           </Content>
         </Container>
       </React.Fragment>
