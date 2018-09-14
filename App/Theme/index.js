03d9c14a96fb7a086961e298a6cb2a04a61eef91
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-09-14 05:02:28 -0700

    Fix Measurements Theme submodule import typo

--- App/Theme/index.js
+++ App/Theme/index.js
@@ -1,13 +1,13 @@
 import Colors from './Colors';
 import Fonts from './Fonts';
 import Images from './Images';
-import Measurments from './Measurments';
+import Measurements from './Measurements';
 import Styles from './Styles';
 
 export {
   Colors,
   Fonts,
   Images,
-  Measurments,
+  Measurements,
   Styles
 };
