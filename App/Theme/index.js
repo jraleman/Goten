452d4fd86119332c14f6f9be4901c14f3a099a90
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-09-14 04:48:15 -0700

    Import Theme submodules to index.js

--- App/Theme/index.js
+++ App/Theme/index.js
@@ -1,5 +1,8 @@
 import Images from './Images';
 import Colors from './Colors';
+import Fonts from './Fonts';
+import Styles from './Styles';
+import Measurments from './Measurments';
 
 export {
   Colors,
