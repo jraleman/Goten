df5288cf878196f1ead5c325ea61d83171dc27a6
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-09-14 04:37:19 -0700

    Refactor Theme modules

---
+++ App/Theme/Images/Images.js
@@ -0,0 +1,13 @@
+const Images = {
+  onboardingSlideOne: require('./Source/slide-one.png'),
+  onboardingSlideTwo: require('./Source/slide-two.png'),
+  onboardingSlideThree: require('./Source/slide-three.png'),
+  onboardingSlideFour: require('./Source/slide-four.png'),
+  loginBackground: require('./Source/login-background.jpg'),
+  logoWhite: require('./Source/logo-white.png'),
+  logoBlack: require('./Source/logo-black.png'),
+  drawerNavigationCover: require('./Source/drawer-navigation-cover.jpg'),
+  drawerNavigationAvatar: require('./Source/drawer-navigation-avatar.png')
+};
+
+export default Images;
--- App/Theme/Images/index.js
+++ App/Theme/Images/index.js
@@ -1,13 +1,3 @@
-const Images = {
-  onboardingSlideOne: require('./Source/slide-one.png'),
-  onboardingSlideTwo: require('./Source/slide-two.png'),
-  onboardingSlideThree: require('./Source/slide-three.png'),
-  onboardingSlideFour: require('./Source/slide-four.png'),
-  loginBackground: require('./Source/login-background.jpg'),
-  logoWhite: require('./Source/logo-white.png'),
-  logoBlack: require('./Source/logo-black.png'),
-  drawerNavigationCover: require('./Source/drawer-navigation-cover.jpg'),
-  drawerNavigationAvatar: require('./Source/drawer-navigation-avatar.png')
-};
+import Images from './Images';
 
 export default Images;
