358b2e9f1243c1f3ba200b6cd34d54124ef13fa1
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-09-15 02:55:04 -0700

    Rename Config module src file

--- App/Config/index.js
+++ App/Config/index.js
@@ -1,5 +1,6 @@
-// import { Text } from 'react-native';
-// import AppConfig from './AppConfig';
+import { Text } from 'react-native';
+import AppConfig from './AppConfig';
 
 // Allow font-scaling in app
 // Text.defaultProps.allowFontScaling = AppConfig.allowTextFontScaling;
+export AppConfig;
