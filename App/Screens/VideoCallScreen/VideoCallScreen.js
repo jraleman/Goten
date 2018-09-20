333249696953502c9db1065c7c4e13e08e1d0025
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-09-19 18:21:06 -0700

    Add props to video component

--- App/Screens/VideoCallScreen/VideoCallScreen.js
+++ App/Screens/VideoCallScreen/VideoCallScreen.js
@@ -8,9 +8,12 @@ import {
   Content,
   Text,
 } from 'native-base';
+import Video from 'react-native-video';
 // ~~ Local Dependencies ~~
 import styles from './styles';
 import AppHeader from '../../Components/AppHeader';
+
+// const videoSrc = require('../../Assets/Videos/HACKERMAN.mp4')
 
 /**
  * Container to render when DrawerNavigation is called.
@@ -35,15 +38,37 @@ class VideoCallScreen extends React.Component {
           barStyle={ 'light-content' }
           animated={ true }
         />
-        <Container style={ styles.container }>
+        <Container>
           <AppHeader
             title={ 'Video' }
             icon={ 'menu' }
             onPress={ () => { this.props.navigation.openDrawer(); }}
           />
-          <Content style={ styles.content }>
-            <Text>{ "VideoScreen" }</Text>
-          </Content>
+            <Content>
+            <Video
+              source={{ uri: '../../Assets/Videos/HACKERMAN.mp4' }}
+              ref={(ref) => { this.player = ref }}
+              rate={ 1.0 }                              // 0 is paused, 1 is normal.
+              volume={ 1.0 }                            // 0 is muted, 1 is normal.
+              muted={ false }                           // Mutes the audio entirely.
+              paused={ false }                          // Pauses playback entirely.
+              resizeMode="cover"                      // Fill the whole screen at aspect ratio.*
+              repeat={ true }                           // Repeat forever.
+              playInBackground={false}                // Audio continues to play when app entering background.
+              playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
+              ignoreSilentSwitch={"ignore"}           // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent. When 'obey', audio will toggle with the switch. When not specified, will inherit audio settings as usual.
+              progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
+              onLoadStart={(el)=>console.log("video is being loaded",el)}            // Callback when video starts to load
+              onLoad={()=>console.log("video loading")}               // Callback when video loads
+              onProgress={()=>console.log("video loading is in progress")}               // Callback every ~250ms with currentTime
+              onEnd={()=>console.log("video is now loaded")}                      // Callback when playback finishes
+              onError={()=>console.log("video can not be loaded")}               // Callback when video cannot be loaded
+              onBuffer={()=>console.log("buffer stage")}                // Callback when remote video is buffering
+              onTimedMetadata={()=>console.log("metadata received")}  // Callback when the stream receive some metadata
+              style={ styles.backgroundVideo }
+             />
+             </Content>
+
         </Container>
       </React.Fragment>
     );
--- App/Screens/VideoCallScreen/styles.js
+++ App/Screens/VideoCallScreen/styles.js
@@ -19,8 +19,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
-    content: {
+  content: {
     flex: 1
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   }
 });
 
