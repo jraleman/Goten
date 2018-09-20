c552ff283efbba66f24485d65b648c5c62e8cffa
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-09-19 18:22:10 -0700

    Link react-native-video to android project

--- android/app/src/main/java/com/basico/MainApplication.java
+++ android/app/src/main/java/com/basico/MainApplication.java
@@ -3,6 +3,7 @@ package com.basico;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactVideoPackage(),
             new RNI18nPackage(),
             new VectorIconsPackage()
       );
